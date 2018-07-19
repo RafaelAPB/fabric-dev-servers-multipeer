b90d5fa5fb1e054b02389d98ce5b25b0f2adf08a
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2018-07-19 19:53:37 +0100

    Add Orderer start

---
+++ startFabric-Orderer2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ -z ${FABRIC_START_TIMEOUT+x} ]; then
+ echo "FABRIC_START_TIMEOUT is unset, assuming 15 (seconds)"
+ export FABRIC_START_TIMEOUT=15
+else
+
+   re='^[0-9]+$'
+   if ! [[ $FABRIC_START_TIMEOUT =~ $re ]] ; then
+      echo "FABRIC_START_TIMEOUT: Not a number" >&2; exit 1
+   fi
+
+ echo "FABRIC_START_TIMEOUT is set to '$FABRIC_START_TIMEOUT'"
+fi
+
+# Exit on first error, print all commands.
+set -ev
+
+#Detect architecture
+ARCH=`uname -m`
+
+# Grab the current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#
+
+# ARCH=$ARCH docker-compose -f "${DIR}"/composer/docker-compose-peer2.yml down
+ARCH=$ARCH docker-compose -f "${DIR}"/composer/docker-compose-orderer2.yml up -d
+
+# wait for Hyperledger Fabric to start
+# incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
+echo ${FABRIC_START_TIMEOUT}
+sleep ${FABRIC_START_TIMEOUT}
