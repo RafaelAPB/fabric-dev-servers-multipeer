e8cd86843a5169483a19b4cd29c4484fd0afffd6
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2018-09-20 08:51:33 +0100

    Update howtobuild.sh

--- composer/howtobuild.sh
+++ composer/howtobuild.sh
@@ -1,23 +1,35 @@
 cd "$(dirname "$0")"
 
-PEER1="192.168.1.222"
-PEER2="192.168.1.224"
-PEER3="192.168.1.224"
-PEER4="192.168.1.224"
-PEER5="192.168.1.224"
-ORDERER0="192.168.1.224"
-ORDERER1="192.168.1.224"
-ORDERER2="192.168.1.224"
-KAFKA1="192.168.1.224"
-KAFKA2="192.168.1.224"
-KAFKA3="192.168.1.224"
-KAFKA4="192.168.1.224"
-ZKEEPER1="192.168.1.224"
-ZKEEPER2="192.168.1.224"
-ZKEEPER3="192.168.1.224"
+HOST0=""
+HOST1=""
+HOST2=""
+HOST3=""
+HOST4=""
 
-sed -i -e "s/{IP-HOST-2}/$PEER2/g" ../createPeerAdminCard.sh
-sed -i -e "s/{IP-HOST-2}/$PEER2/g" ../createPeerAdminCard.sh
+PEER0=$HOST0" peer0.org1.example.com"
+PEER1=$HOST1" peer1.org1.example.com"
+PEER2=$HOST2" peer2.org1.example.com"
+PEER3=$HOST3" peer3.org1.example.com"
+PEER4=$HOST4" peer4.org1.example.com"
+
+ORDERER0=$HOST1" orderer0.example.com"
+ORDERER1=$HOST2" orderer1.example.com"
+ORDERER2=$HOST3" orderer2.example.com"
+
+ZKEEP0=$HOST1" zookeeper0.example.com"
+ZKEEP1=$HOST3" zookeeper1.example.com"
+ZKEEP2=$HOST4" zookeeper2.example.com"
+
+KAFKA0=$HOST0" kafka0.example.com"
+KAFKA1=$HOST0" kafka1.example.com"
+KAFKA2=$HOST0" kafka2.example.com"
+KAFKA3=$HOST0" kafka3.example.com"
+
+sed -i -e "s/{HOST-0}/$HOST0/g" ../createPeerAdminCard.sh
+sed -i -e "s/{HOST-1}/$HOST1/g" ../createPeerAdminCard.sh
+sed -i -e "s/{HOST-2}/$HOST2/g" ../createPeerAdminCard.sh
+sed -i -e "s/{HOST-3}/$HOST3/g" ../createPeerAdminCard.sh
+sed -i -e "s/{HOST-4}/$HOST4/g" ../createPeerAdminCard.sh
 
 cryptogen generate --config=./crypto-config.yaml
 export FABRIC_CFG_PATH=$PWD
@@ -36,3 +48,17 @@ sed -i -e "s/{ORG3-CA-KEY}/$ORG3KEY/g" docker-compose-peer3.yml
 sed -i -e "s/{ORG4-CA-KEY}/$ORG4KEY/g" docker-compose-peer4.yml
 sed -i -e "s/{ORG5-CA-KEY}/$ORG5KEY/g" docker-compose-peer5.yml
 
+sudo echo $PEER0 >> /etc/hosts
+sudo echo $PEER1 >> /etc/hosts
+sudo echo $PEER2 >> /etc/hosts
+sudo echo $PEER3 >> /etc/hosts
+sudo echo $ORDERER0 >> /etc/hosts
+sudo echo $ORDERER1 >> /etc/hosts
+sudo echo $ORDERER2 >> /etc/hosts
+sudo echo $ZKEEP0 >> /etc/hosts
+sudo echo $ZKEEP1 >> /etc/hosts
+sudo echo $ZKEEP2 >> /etc/hosts
+sudo echo $KAFKA0 >> /etc/hosts
+sudo echo $KAFKA1 >> /etc/hosts
+sudo echo $KAFKA2 >> /etc/hosts
+sudo echo $KAFKA3 >> /etc/hosts
